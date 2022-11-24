c24d23dec79d35898fbe320e4cd5f96d878c9e2d
Warren-Ipro <warren@iprogroup.ca>
2022-11-23 23:24:14 -0800

    Connected to strapi api.

--- pages/projects.js
+++ pages/projects.js
@@ -1,11 +1,33 @@
 import Layout from '../components/Layout';
+import { fetcher } from '../lib/api';
 
-const Projects = () => {
+const Projects = ({ websites }) => {
   return (
     <Layout>
-      <h1>Projects</h1>
+      <h1 className='font-bold text-5xl'>Projects</h1>
+      <div className="">
+        {
+          websites.data.map((website, i) => {
+            return (
+              <div key={i}>
+                {website.attributes.title}
+              </div>
+            )
+          })
+        }
+      </div>
     </Layout>
   )
 }
 
+export async function getStaticProps() {
+  const websiteResponse = await fetcher(`${process.env.NEXT_PUBLIC_STRAPI_URL}/websites`);
+  return {
+    props: {
+      websites: websiteResponse
+    }
+  }
+}
+
 export default Projects
+
