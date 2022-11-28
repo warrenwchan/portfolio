01ca0e203de0f0d49f07d96df72cdee52d8eedda
warrenwchan <w.warrenchan@gmail.com>
2022-11-28 13:14:30 -0800

    Added Vercel Analytics.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Analytics } from '@vercel/analytics/react';
 import { Inter } from '@next/font/google'
 import { config } from '@fortawesome/fontawesome-svg-core'
 import '@fortawesome/fontawesome-svg-core/styles.css'
@@ -11,6 +12,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <div className={`${inter.className} h-full`}>
       <Component {...pageProps} />
+      <Analytics />
     </div>
   )
 }
