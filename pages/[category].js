109826b0cdca0af44955da220031b9096cc7837f
warrenwchan <w.warrenchan@gmail.com>
2022-11-29 02:16:30 -0800

    adjusted category style.

--- pages/[category].js
+++ pages/[category].js
@@ -51,7 +51,7 @@ const Category = ({ categoryObject }) => {
                     </div>
                     <div className="flex-grow">
                       <h2 className="font-medium mb-1">{project.attributes.title}</h2>
-                      <div className="flex flex-row gap-4 justify-start items-start">
+                      <div className="flex flex-row gap-4 justify-between items-start">
                         <p className="text-slate-600 leading-normal text-sm">{project.attributes.description}</p>
                         <button className="pl-6 pr-3 py-2 text-xs font-medium bg-slate-100 group-hover:bg-slate-200/50 transition-all ease-out duration-200 rounded-full flex flex-row items-center gap-x-1">View <FontAwesomeIcon icon={faArrowRight} className="scale-75 transition transform opacity-0 -translate-x-4 group-hover:-translate-x-0 group-hover:opacity-100" /></button>
                       </div>
