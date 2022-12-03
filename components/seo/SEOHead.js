46d139f15dad3bc79f9c60467a9edb52b9e8f77e
warrenwchan <w.warrenchan@gmail.com>
2022-12-03 13:28:15 -0800

    fixed title Meta

--- components/seo/SEOHead.js
+++ components/seo/SEOHead.js
@@ -16,7 +16,7 @@ export default function Home({
     	{/*
         	Fundamental head elements important for SEO.
     	*/}
-    	<title>{ title } </title>
+    	<title>{ title }</title>
     	<meta name="description" content={description} />
     	<link rel="canonical" href={canonicalUrl} />
     	<meta name="viewport" content="width=device-width, initial-scale=1" />
